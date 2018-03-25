1c4a9f910196a3d06cfc642d7ca413ac9ce4abb8
Yu Hua Cheng <ycheng@pulsepoint.com>
2018-03-24 22:07:31 -0400

    reverse string

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,33 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+function reverse(str) {
+    return str
+        .split('')
+        .reduce((reversed, character) => character + reversed, '')
+}
 
 module.exports = reverse;
+
+// // solution 1
+// function reverse(str) {
+//     const strLength = str.length;
+//
+//     let reversedString = '';
+//     let i = 1;
+//     while(i <= strLength){
+//         reversedString += str[strLength - i];
+//         i++;
+//     }
+//     return reversedString;
+// }
+//
+// // solution 2
+// function reverse(str) {
+//     let reversed = '';
+//
+//     for (let char of str){
+//         reversed = char + reversed;
+//     }
+//     return reversed
+// }
