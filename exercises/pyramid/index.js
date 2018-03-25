425e9871bc61856ca8b0a8c5948b97488e4972c4
Yu Hua Cheng <ycheng@pulsepoint.com>
2018-03-25 14:10:06 -0400

    pyramid

--- exercises/pyramid/index.js
+++ exercises/pyramid/index.js
@@ -14,6 +14,44 @@
 //       ' ### '
 //       '#####'
 
-function pyramid(n) {}
+function pyramid(n, row=0, level='') {
+    if (n === row){
+        return;
+    }
+
+    const colLength = 2 * n - 1;
+    if (level.length === colLength){
+        console.log(level);
+        return pyramid(n, row + 1)
+    }
+
+    const centerIndex = Math.floor(colLength / 2);
+    let add;
+    if (centerIndex - row <= level.length && centerIndex + row >= level.length){
+        add = '#';
+    } else {
+        add = ' ';
+    }
+    pyramid(n, row, level + add)
+}
+
+// function pyramid(n) {
+//     for (let row = 0; row < n; row++){
+//         let level = '';
+//         const colLength = 2 * n - 1; // hardest part is to figure out this 2*n - 1
+//         const centerIndex = Math.floor(colLength / 2);
+//
+//         for (let col = 0; col < colLength; col++){
+//             if (centerIndex - row <= col && centerIndex + row >= col){
+//                 level += '#';
+//             }
+//             else {
+//                 level += ' ';
+//             }
+//         }
+//         console.log(level);
+//     }
+// }
+
 
 module.exports = pyramid;
