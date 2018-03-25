2ed3fbc335da95aa5add7425cb7653b08707cf8c
Yu Hua Cheng <ycheng@pulsepoint.com>
2018-03-25 13:02:21 -0400

    chunk

--- exercises/fizzbuzz/index.js
+++ exercises/fizzbuzz/index.js
@@ -21,7 +21,7 @@ function fizzBuzz(n) {
             console.log('fizz')
         } else if (i % 5 ===0){
             console.log('buzz')
-        }         
+        }
         else {
             console.log(i)
         }
