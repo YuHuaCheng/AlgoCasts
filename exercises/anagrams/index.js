99a149b53e03d791902c7ce8febafcc36db13b89
Yu Hua Cheng <ycheng@pulsepoint.com>
2018-03-25 13:16:27 -0400

    anagrams

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,33 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+function anagrams(stringA, stringB) {
+    const aCharMap = buildCharMap(stringA);
+    const bCharMap = buildCharMap(stringB);
+
+    if (Object.keys(aCharMap).length !== Object.keys(bCharMap).length){
+        return false
+    }
+
+    for (let char in aCharMap){
+        if (aCharMap[char] !== bCharMap[char]){
+            return false
+        }
+    }
+    return true
+}
+
+function buildCharMap(str){
+    const charMap = {};
+
+    const strParsed = str.replace(/[^\w]/g, '').toLowerCase();
+    for (let char of strParsed){
+        charMap[char] = charMap[char] + 1 || 1;
+    }
+    return charMap
+}
+
+// solution 2
+// split stringA and stringB to 2 arrays, then sort each them, then compare
 
 module.exports = anagrams;
